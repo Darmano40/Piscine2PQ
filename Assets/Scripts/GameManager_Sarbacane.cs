7b03c483d27a9b8691f77e0e53eab12dd2945e7b
MSI\Tjtaz <36964563+Tjtaz@users.noreply.github.com>
2018-09-18 11:51:29 +0200

    gm_sabacane maj
    c'est pas encore fonctionnel mais ok

--- Assets/Scripts/GameManager_Sarbacane.cs
+++ Assets/Scripts/GameManager_Sarbacane.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager_Sarbacane : MonoBehaviour {
+    public int Return;
+    public int Level_End;
+    public UIManager UIManager_Sarbacane;
+    public SoundManager SoundManager_Sarbacane;
 
-	// Use this for initialization
-	void Start () {
+        private void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Singleton = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
