c763a31eb3c4364d8f4b820e1e4755e66258948d
size45 <33767630+size45@users.noreply.github.com>
2018-09-18 13:11:08 +0200

    Respawn Ball
    Respawn Ball

---
+++ Assets/Scripts/Basket_Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Basket_Ball : MonoBehaviour{
+
+    private Vector2 origTrans;
+
+    void Start()
+    {
+      
+        origTrans = transform.position;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Basket_Trash")
+        {
+            transform.position = origTrans;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+        
+    }
+}
+
+
