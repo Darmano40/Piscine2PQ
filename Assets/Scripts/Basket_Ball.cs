a3e56b74d2eb7e784b73f86f928344de27560bd3
size45 <33767630+size45@users.noreply.github.com>
2018-09-19 15:12:33 +0200

    Add VFX win/loose et sfx launched

--- Assets/Scripts/Basket_Ball.cs
+++ Assets/Scripts/Basket_Ball.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Basket_Ball : MonoBehaviour{
+public class Basket_Ball : MonoBehaviour {
 
     private Vector2 Respawn_Ball;
     public AudioClip Basket_OK;
     public AudioClip Basket_Fail;
     public GameObject img_win;
     public GameObject img_Loose;
+    public AudioClip Basket_throw;
+    public bool Launched;
+
 
 
 
     void Start()
     {
+        Launched = false;
 
         Respawn_Ball = transform.position;
 
     }
+    private IEnumerator Waiting_Img()
+    {
+
+        yield return new WaitForSeconds(4f);
+        img_win.SetActive(false);
+        img_Loose.SetActive(false);
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.E)&& Launched == false) {
+            Launched = true;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            Sound_Manager_Basket.instance.RandomizeSfx(Basket_throw);
+            
+
+        }
+    }
+
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,17 +53,26 @@ public class Basket_Ball : MonoBehaviour{
             Sound_Manager_Basket.instance.RandomizeSfx(Basket_OK);
             transform.position = Respawn_Ball;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                
+            StartCoroutine(Waiting_Img());
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            Launched = false;
+
 
         }
         else if (collision.gameObject.tag == "Basket_Ground")
         {
+            img_Loose.SetActive(true);
             Sound_Manager_Basket.instance.RandomizeSfx(Basket_Fail);
             transform.position = Respawn_Ball;
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            StartCoroutine(Waiting_Img());
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+            Launched = false;
         }
 
     }
+
+
 }
 
 
