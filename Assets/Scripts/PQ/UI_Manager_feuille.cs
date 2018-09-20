827e2b9dc905591932e10a8884360cd679cab4a1
Vil_Coyote <ghost64400@gmail.com>
2018-09-20 12:41:27 +0200

    Fix feuille + ajout rouleau (PQ)

--- Assets/Scripts/PQ/UI_Manager_feuille.cs
+++ Assets/Scripts/PQ/UI_Manager_feuille.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Manager_feuille : MonoBehaviour {
+public class UI_Manager_feuille : MonoBehaviour
+{
 
+    public Image roll;
     public Image leaf;
+
     public float draggedleaf = 0.2f;
-    public float leafState = 0.0f;
-    public float filledLeaf = 1.0f;
+    public float leafState = 0.2f;
+    public float filledLeaf = 1.2f;
+
+    public float tearedLeaves = 1.0f;
+    public float totalLeavesPerRoll = 30.0f;
 
 	void Start ()
     {
-        RollOnce();
+        UpdateLeafDisplay();
     }
 	
 	void Update ()
@@ -28,7 +34,8 @@ public class UI_Manager_feuille : MonoBehaviour {
         leafState += draggedleaf;
         if (EntireLeaf())
         {
-           // Décrocher la feuille avec "tear"
+            // Décrocher la feuille avec "tear"
+            tearedLeaves++;
         }
         UpdateLeafDisplay();
     }
@@ -41,8 +48,15 @@ public class UI_Manager_feuille : MonoBehaviour {
     public void UpdateLeafDisplay()
     {
         leaf.fillAmount = leafState;
-        Debug.Log("Sprite Updated");
+        Debug.Log("Leaf Sprite Updated");
     }
+
+    public void UpdateRollDisplay(int tearedLeaves)
+    {
+        roll.fillAmount = (totalLeavesPerRoll * 1.0f) / (tearedLeaves * 1.0f);
+        Debug.Log("Roll Sprite Updated");
+    }
+
 }
 
 
