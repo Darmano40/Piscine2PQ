c89c3c42b5e776b4108e530ce0c2ed38c115504b
MSI\Nico <lepilolodior7633@gmail.com>
2018-09-21 14:37:35 +0200

    no message

--- Assets/Scripts/Drague/NewBehaviourScript.cs
+++ Assets/Scripts/Drague/NewBehaviourScript.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public Image target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    protected string word;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Fillamount()
+    {
+        target.fillAmount += 0.1f;
+    }
 }
