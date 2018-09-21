34c625dd0640723a18779dd4c4246cbb40c1a169
DESKTOP-GHB1A7L\Paulo <paulo-77@live.fr>
2018-09-21 10:58:55 +0200

    Ajout_Ui_Scene_Drague_02

---
+++ Assets/Scripts/Drague/PauseButton_Test_Semi_Fonctionnel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour {
+
+    public bool PauseActive;
+    public GameObject Button;
+
+	// Use this for initialization
+	void Start () {
+        PauseActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PauseBouton ()
+    {
+        if ( PauseActive = true ) {
+            Time.timeScale = 0;
+            Button.SetActive(true);
+            PauseActive = true;
+        }
+            
+        if ( PauseActive = false )
+        {
+            Time.timeScale = 1;
+            Button.SetActive(false);
+            PauseActive = false;
+        }
+        
+
+    }
+}
