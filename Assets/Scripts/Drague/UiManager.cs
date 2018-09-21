f5ff929d281f5415fde395223066191e857e6a10
MSI\Nico <lepilolodior7633@gmail.com>
2018-09-21 15:46:22 +0200

    Pause drague

--- Assets/Scripts/Drague/UiManager.cs
+++ Assets/Scripts/Drague/UiManager.cs
@@ -8,22 +8,62 @@ using UnityEngine.SceneManagement;
 public class UiManager : MonoBehaviour
 {
     public GameObject Pause;
+    public Image LoveBarre;
+    public GameObject Victory;
+    public GameObject BP;
+    public GameObject BU;
+    public bool PaoseouPas = false;
 
 
     void Start()
     {
-        
+        LoveBarre.fillAmount = 0.1f;
     }
         
     void Update ()
     {
-        
+        Victoire();
     }
+
+    public void Victoire()
+    {
+        if (LoveBarre.fillAmount >= 1f)
+        {
+            Victory.SetActive(true);
+        }
+
+    }
+    
+
+    public void Love()
+    {
+        if (PaoseouPas == false)
+        { 
+        LoveBarre.fillAmount += 0.1f;
+        Debug.Log(LoveBarre.fillAmount);
+        }
+    }
+    
 
     public void PauseGame()
     {
         Pause.SetActive(true);
+        BP.SetActive(false);
+        BU.SetActive(true);
+        PaoseouPas = true;
     }
+
+    public void ResumeGame()
+    {
+        Pause.SetActive(false);
+        BP.SetActive(true);
+        BU.SetActive(false);
+        PaoseouPas = false;
+
+    }
+
+
+
 
     public void Quit()
     {
