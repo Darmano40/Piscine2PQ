65de2e852c860e1a0a4f2d78c5b1ac1143b08a2e
WarZicks <36964591+WarZicks@users.noreply.github.com>
2018-09-21 15:35:35 +0200

    reload_scene

--- Assets/Scripts/Sarbacane/PauseManager.cs
+++ Assets/Scripts/Sarbacane/PauseManager.cs
@@ -6,17 +6,13 @@ using UnityEngine.SceneManagement;
 public class PauseManager : MonoBehaviour {
 
     public GameObject Pause_Buttons, Timer, Pause_Icon, Shoot_Button;
-    public GameObject[] BD_Cibles;
-    public GameObject[] Angry_Cibles;
     public UI_MAnager my_UIM;
     public GameManager_Sarbacane my_GM;
-    private Scene actualScene;
     public GameObject End_Buttons;
     public bool is_Paused;
 
 	// Use this for initialization
 	void Start () {
-        actualScene = SceneManager.GetActiveScene();
 	}
 	
 	// Update is called once per frame
@@ -42,29 +38,7 @@ public class PauseManager : MonoBehaviour {
 
     public void Retry()
     {
-        Time.timeScale = 1;
-        my_UIM.ActualScore = 0;
-        my_UIM.score.text = "" + 0;
-        my_UIM.time = my_UIM.startTime;
-        my_UIM.t = my_UIM.time * 60;
-        Pause_Buttons.SetActive(false);
-        End_Buttons.SetActive(false);
-        Timer.SetActive(true);
-        Pause_Icon.SetActive(true);
-        Shoot_Button.SetActive(true);
-        my_UIM.score.color = Color.black; ;
-        my_UIM.score.fontSize = 95;
-        my_UIM.scoreOutline.effectColor = Color.white;
-        for (int i = 0; i < BD_Cibles.Length; i++)
-        {
-            BD_Cibles[i].SetActive(false);
-        }
-        for (int i = 0; i < Angry_Cibles.Length; i++)
-        {
-            Debug.Log("angry_returns");
-            Angry_Cibles[i].SetActive(false);
-        }
-
+        SceneManager.LoadScene(Application.loadedLevel);
     }
 
     public void Menu()
